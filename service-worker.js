8e4c1b2524709c6f1fd230ed3aeac9a29467f3e5
minamar7 <139824491+minamar7@users.noreply.github.com>
2026-01-25 22:23:37 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,49 @@
+const STATIC_CACHE = "ssp-static-v4";
+const DYNAMIC_CACHE = "ssp-dynamic-v1";
+
+/* 
+  ΣΗΜΑΝΤΙΚΟ:
+  Στο GitHub Pages το site ζει μέσα σε φάκελο.
+  Άρα ΟΛΑ τα paths πρέπει να είναι relative ("./").
+*/
+
+const STATIC_ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./quiz.js",
+  "./i18n.js",
+  "./styles.css",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+/* INSTALL */
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+/* ACTIVATE */
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== STATIC_CACHE && key !== DYNAMIC_CACHE)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+/* FETCH */
 self.addEventListener("fetch", (event) => {
   if (!event.request.url.startsWith(self.location.origin)) {
     return;
